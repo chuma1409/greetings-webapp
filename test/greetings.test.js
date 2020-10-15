87b7f1f3385c6fadc27cd6d794a5a0782a0fb8ad
chuma1409 <chumagushman14@gmail.com>
2020-10-15 15:45:17 +0200

    removed unused tests nd fied tests, aligned names

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -14,47 +14,21 @@ describe("Greeting", function () {
     const pool = new Pool({
         connectionString
     });
-    const INSERT_QUERY = "insert into greet (name, counter) values ($1, $2)";
+    
 
     beforeEach(async function () {
 		await pool.query("delete from greet");
 	});
 
-// it("should be able to add users  greeted", async function(){
-//     await pool.query(INSERT_QUERY,["esihle", 2]);
-//     await pool.query(INSERT_QUERY,["qwerty", 1]);
 
-//     const results = await pool.query("select count(*) from greet");
 
-//     assert.equal(2, results.rows[0].count);
-// })
-it("should be able to find a greeted name", async function(){
-     await pool.query(INSERT_QUERY,["esihle", 2]);
-
-    const results = await pool.query("select * from greet where name=$1", ["esihle"]);
-
-    assert.equal("esihle", results.rows[0].name);
-	assert.equal(2, results.rows[0].counter);
-})
 it("should be able to greet Chuma and Lineo once",async function () {
     let greetings = Greetings(pool)
    await greetings.setGreetNames("Chuma")
    await greetings.setGreetNames("lineo")
     assert.equal(2, await greetings.greetNameCounter());
   });
-// it("should be able to count all greeted users", async function(){
-//     await pool.query(INSERT_QUERY,["chuma", 2]);
-//     await pool.query(INSERT_QUERY,["qwerty", 7]);
-//     await pool.query(INSERT_QUERY,["esihle", 9]);
-//     await pool.query(INSERT_QUERY,["musa", 6]);
-//     await pool.query(INSERT_QUERY,["sibo", 8]);
-//     await pool.query(INSERT_QUERY,["loreen", 5]);
 
-//     const results = await pool.query("select count(*) from greet")
-
-//     assert.equal(6, results.rows[0].count);
-
-// })
 it("count names stored in local storage", async function () {
     let greetings = Greetings(pool)
    await greetings.greetNameCounter()
